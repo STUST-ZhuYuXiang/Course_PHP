31b1ca96fe47a201da20015d91cbb3f8cbfddfd2
ZhuYuXiang <46668036+STUST-ZhuYuXiang@users.noreply.github.com>
2020-04-16 20:51:22 +0800

    Update PHA01-2.php

--- 20200325/php101/PHA01-2.php
+++ 20200325/php101/PHA01-2.php
@@ -20,19 +20,18 @@
 </head>
 
 <body>
-  <embed src=<?php echo $_POST["music"]; ?> width=0 height=0>
+  <embed src=<?php $_POST["music"]; ?> width=0 height=0>
   <div align="center">
     <p><img src="images/php01-6.gif" width="466" height="59" /></p>
   </div>
-  <p class="style3">來自<a href="mailto:<?php echo $_POST["form_name"]; ?>"><?php ?></a>的祝福：</p>
+  <p class="style3">來自<a href="mailto:<?php $_POST["from_email"]; ?>"><?php $_POST["from_name"]; ?></a>的祝福：</p>
   <table width="80%" border="0" align="center" cellpadding="10" cellspacing="0">
     <tr>
-      <!--接收php01-1.php 的 name="cardtype" 值-->
-      <td colspan="3" align="center"><?php echo "<img src='" . $_POST["cardtype"] . "'>"; ?></td>
+      <td colspan="3" align="center"><?php echo "<img src='images/".$_POST["cardtype"]."'/>";  ?></td>
     </tr>
     <tr>
       <td height="40" colspan="2" align="left" bordercolor="1" bgcolor="#CCFFFF">
-        <p align="left"><span class="style3">賀卡標題：<?php echo $_POST["subject"]; ?></span> <br />
+        <p align="left"><span class="style3">賀卡標題：<?php $_POST["subject"]; ?></span> <br />
         </p>
       </td>
       <td align="right" bordercolor="1" bgcolor="#CCFFFF"><span class="style3">
@@ -46,9 +45,7 @@
     </tr>
     <tr>
       <td width="100" valign="top" bordercolor="1" bgcolor="#66FF99">&nbsp;</td>
-      <td valign="top" bordercolor="1" bgcolor="#66FF99">
-        <span class="style5" style="font-style:<?php echo $_POST["style"]; ?>;color:<?php echo $_POST["color"]; ?>;font-size:<?php echo $_POST["size"]; ?>;"><?php echo nl2br($_POST["message"]); ?></span>
-      </td>
+      <td valign="top" bordercolor="1" bgcolor="#66FF99"><span class="style5" style="font-style:<?php echo $_POST["style"]; ?>;color:<?php echo $_POST["color"]; ?>;font-size:<?php echo $_POST["size"]; ?>;"><?php nl2br($_POST["message"]); ?></span></td>
       <td width="100" valign="top" bordercolor="1" bgcolor="#66FF99">&nbsp;</td>
     </tr>
   </table>
