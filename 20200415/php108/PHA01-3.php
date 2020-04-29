ee0c868dff36440f2a04caa8fbdf3c7a71253df7
ZhuYuXiang <46668036+STUST-ZhuYuXiang@users.noreply.github.com>
2020-04-29 10:20:56 +0800

    Update PHA01-3.php

--- 20200415/php108/PHA01-3.php
+++ 20200415/php108/PHA01-3.php
@@ -5,30 +5,30 @@
 </head>
 
 <body>
-   <?php
+	<?php
       $t1 = $_GET["t1"];
       $t2 = $_GET["t2"];
       $t3 = $_GET["t3"];
-		$nochk = $_GET["nochk"];
-		
-		if (empty($t1) || empty($t2) || empty($t3)) {
-			$outstr = "第";
-			if (empty($t1))
-				$outstr .= "一";
-			if (empty($t2))
-				$outstr .= "二";
-			if (empty($t3))
-				$outstr .= "三";
-			$outstr .= "文字方塊必須輸入文字<br/>";
-		} 
-		else
-			$outstr = "";
+			$nochk = $_GET["nochk"];
+	
+			if (empty($t1) || empty($t2) || empty($t3)) {
+				$outstr = "第";
+				if (empty($t1))
+					$outstr .= "一";
+				if (empty($t2))
+					$outstr .= "二";
+				if (empty($t3))
+					$outstr .= "三";
+				$outstr .= "文字方塊必須輸入文字<br/>";
+			} 
+			else
+				$outstr = "";
 
-		if ($nochk == 1)
-			$outstr .= "至少勾選1個或1個以上核選方塊";
+			if ($nochk == 1)
+				$outstr .= "至少勾選1個或1個以上核選方塊";
 
-		echo $outstr;
-   ?>
+			echo $outstr;
+   	?>
 
    <form action="PHA01-1.PHP" method="post">
       <input type="submit" value="回上一頁">
