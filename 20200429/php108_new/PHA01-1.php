65fc3c6f7a8462df01746532d485acda484b0a38
ZhuYuXiang <46668036+STUST-ZhuYuXiang@users.noreply.github.com>
2020-05-04 16:44:17 +0800

    Create PHA01-1.php

---
+++ 20200429/php108_new/PHA01-1.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+	<meta http-equiv="Content-Language" content="zh-tw">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>3個數字的運算式</title>
+</head>
+
+<body>
+
+	<p align="center">
+		<h2>3個數字的運算式</h2>
+	</p>
+	<form action="PHA01-2.PHP" method="post">
+		<p>請輸入第1個數字:<input type="text" name="T1" size="10"></p>
+		<p>請輸入第2個數字:<input type="text" name="T2" size="10"></p>
+		<p>請輸入第3個數字:<input type="text" name="T3" size="10"></p>
+		<p>
+			<input name="op[]" type="checkbox" value="+">三數相加&nbsp;&nbsp;
+			<input name="op[]" type="checkbox" value="-">三數相減&nbsp;
+			<input name="op[]" type="checkbox" value="*">三數相乘&nbsp;
+			<input name="op[]" type="checkbox" value="/">三數相除
+		</p>
+		<p><input type="submit" value="送出" name="B1">&nbsp;&nbsp;<input type="reset" value="重新設定" name="B2"></p>
+	</form>
+
+</body>
+
+</html>
