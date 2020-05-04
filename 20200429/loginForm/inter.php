f08b09d77304a59c3cf9692e39408bdd96f33317
ZhuYuXiang <46668036+STUST-ZhuYuXiang@users.noreply.github.com>
2020-05-04 17:04:03 +0800

    Create inter.php

---
+++ 20200429/loginForm/inter.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+
+	<!--匯入樣式檔-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+</head>
+
+<body>
+	<form id="form1" action="login.php" method="post">
+		<div class="container-fluid">
+			<div class="form-row">
+				<p><h1>已登入頁面</h1></p>
+			</div>
+		</div>
+	</form>
+</body>
+
+</html>
